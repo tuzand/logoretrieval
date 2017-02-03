a958795b813d748336eb9cfbce00fea6309742d0
Andras Tuzko <tuzand@github.com>
2017-02-03 16:18:18 +0100

    split method also for det images

--- split_IM_BB_FlickrLogos.py
+++ split_IM_BB_FlickrLogos.py
@@ -1,57 +1,76 @@
 from shutil import copy2
 import os.path
+import re
+
+bboxes = 'Annotations'
+images = 'Images'
+imageSets = 'ImageSets'
+
+detectionSuffix = '_det'
+
+datasetPath = "/home/andras/data/datasets/FL32/FlickrLogos-v2"
+outputPathSimple = os.path.join(datasetPath, 'fl', 'fl_devkit', 'data')
+outputPathDetection = os.path.join(datasetPath, 'fl', 'fl_devkit_detection', 'data')
 
 
-dataSetPath = "/home/andras/data/datasets/FL32/FlickrLogos-v2"
-outputPathDetection = "/home/andras/data/datasets/FL32/FlickrLogos-v2/split_detection"
-outputPathSimple = "/home/andras/data/datasets/FL32/FlickrLogos-v2/split"
-bbOutputPath = os.path.join(dataSetPath, 'fl', 'fl_devkit', 'data', 'Annotations')
-
-def extractImages(fileListPath, phase, nologo, detection, abs):
-    if detection == True:
+def extractImages(fileListPath, phase, detection):
+    if detection:
         outputPath = outputPathDetection
     else:
         outputPath = outputPathSimple
     with open(fileListPath, "r") as fileList:
         lines = fileList.read().splitlines()
+        imageList = ''
         for f in lines:
-            splittedPath = f.split('/')
-            dirName = splittedPath[-2]
-            if ( (dirName != 'no-logo') or (nologo == True) ):
-                file = splittedPath[-1]
-                if not os.path.exists(outputPath + "/" + phase + "/" + dirName):
-                    os.makedirs(outputPath + "/" + phase + "/" + dirName)
-                copy2(dataSetPath + "/" + f, outputPath + "/" + phase + "/" + dirName)
-                if dirName != 'no-logo':
-                    if not os.path.exists(bbOutputPath):
-                        os.makedirs(bbOutputPath)                
-                    with open(dataSetPath + "/classes/masks/"+ dirName + "/" + file + ".bboxes.txt", 'r') as bbFile:
-                        data = bbFile.read()
-                    import re
-                    objs = re.findall('\d+ \d+ \d+ \d+', data)
-                    with open(os.path.join(bbOutputPath, file + '.bboxes.txt'), 'w') as bb:
-                        #bb.write("x y width height\n")
-                        for ix, obj in enumerate(objs):
-                            # Make pixel indexes 0-based
-                            coor = re.findall('\d+', obj)
-                            x1 = coor[0]
-                            y1 = coor[1]
-                            w = coor[2]
-                            h = coor[3]
-                            x2 = str(int(coor[0]) + int(w))
-                            y2 = str(int(coor[1]) + int(h))
+            splitPath = f.split('/')
+            brand = splitPath[-2]
+            file = splitPath[-1]
+            orig_file = file
+            if detection:
+                file = file.split('.')[0] + detectionSuffix + '.jpg'
+
+            imageOutPath = os.path.join(outputPath, images)
+            if not os.path.exists(imageOutPath):
+                os.makedirs(imageOutPath)
+            copy2(os.path.join(datasetPath, f), os.path.join(imageOutPath, file))
+
+            imageList += file + '\n'
+
+            bbOutputPath = os.path.join(outputPath, bboxes)
+            if not os.path.exists(bbOutputPath):
+                os.makedirs(bbOutputPath)
+            if brand != 'no-logo':
+                with open(os.path.join(datasetPath, 'classes', 'masks', brand, orig_file + '.bboxes.txt'), 'r') as bbFile:
+                    data = bbFile.read()
+                objs = re.findall('\d+ \d+ \d+ \d+', data)
+                with open(os.path.join(bbOutputPath, file + '.bboxes.txt'), 'w') as bb:
+                    for ix, obj in enumerate(objs):
+                        coor = re.findall('\d+', obj)
+                        x1 = coor[0]
+                        y1 = coor[1]
+                        w = coor[2]
+                        h = coor[3]
+                        x2 = str(int(coor[0]) + int(w))
+                        y2 = str(int(coor[1]) + int(h))
+                        bb.write(str(x1) + " " + str(y1) + " " + str(x2) + " " + str(y2) + "\n")
                         
-                        if abs == True:
-                            bb.write(str(x1) + " " + str(y1) + " " + str(x2) + " " + str(y2) + "\n")
-                        else:
-                            bb.write(str(x1) + " " + str(y1) + " " + str(w) + " " + str(h) + "\n")
-                        if detection == True:
-                            bb.write('logo\n')
-                        else:
-                            bb.write(dirName + '\n')
+                    if detection:
+                        bb.write('logo\n')
+                    else:
+                        bb.write(brand + '\n')
+        if not os.path.exists(os.path.join(outputPath, imageSets)):
+            os.makedirs(os.path.join(outputPath, imageSets))
+        with open(os.path.join(outputPath, imageSets, phase), 'w') as imset:
+            imset.write(imageList)
 
 
 
-extractImages(dataSetPath + "/testset.relpaths.txt", "test", nologo=True, detection = False, abs = False)
-extractImages(dataSetPath + "/trainset.relpaths.txt", "train", nologo=False, detection = False, abs = False)
-extractImages(dataSetPath + "/valset.relpaths.txt", "val", nologo=False, detection = False, abs = False)
+extractImages(datasetPath + '/testset-logosonly.relpaths.txt', 'test_logo.txt', detection = False)
+extractImages(datasetPath + '/testset.relpaths.txt', 'test.txt', detection = False)
+extractImages(datasetPath + '/trainset.relpaths.txt', 'train.txt', detection = False)
+extractImages(datasetPath + '/trainvalset.relpaths.txt', 'trainval.txt', detection = False)
+
+extractImages(datasetPath + '/testset-logosonly.relpaths.txt', 'test_logo.txt', detection = True)
+extractImages(datasetPath + '/testset.relpaths.txt', 'test.txt', detection = True)
+extractImages(datasetPath + '/trainset.relpaths.txt', 'train.txt', detection = True)
+extractImages(datasetPath + '/trainvalset.relpaths.txt', 'trainval.txt', detection = True)
